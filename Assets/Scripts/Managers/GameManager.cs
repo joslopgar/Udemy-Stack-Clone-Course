4cbe447bb4fea811d057a38b37bcbf3867af606c
Jose <jlgarcia92@gmail.com>
2020-07-19 19:35:11 +0200

    Are all managers ready to be used?

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    [HideInInspector] public static bool allManagersAreReadyToBeUsed;
 
     private void Start()
     {
@@ -17,5 +18,32 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
+
+        StartCoroutine(AllManagersReady());
+    }
+
+    /// <summary>
+    /// Are all managers ready to be used?
+    /// Sets the "allManagersAreReadyToBeUsed" variable accordingly
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator AllManagersReady()
+    {
+        // If all managers are already ready to be used, get out of the coroutine immediately
+        if (allManagersAreReadyToBeUsed)
+        {
+            yield break;
+        }
+
+        // Otherwise wait until they are ready, and set "allManagersAreReadyToBeUsed" equal to true
+        while (!allManagersAreReadyToBeUsed)
+        {
+            allManagersAreReadyToBeUsed = BlocksManager.Instance != null && StatsManager.Instance != null &&
+                                          InputManager.Instance != null && AudioManager.Instance != null &&
+                                          UIManager.Instance != null;
+            yield return null;
+        }
+
+        allManagersAreReadyToBeUsed = true;
     }
 }
